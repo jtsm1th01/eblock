084219ece75ae4dd4300fb5d706c62d4a837b182
Dave Guck <davidguck@yahoo.com>
2015-04-21 18:02:00 +0000

    create user model | add validations and associations

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  validates :fname, presence: true
+  validates :lname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sesitive: false }
+  
+  has_many :items
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421174449) do
+ActiveRecord::Schema.define(version: 20150421175519) do
 
   create_table "auctions", force: true do |t|
     t.integer  "charity_id"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20150421174449) do
 
   create_table "charities", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "fname"
+    t.string   "lname"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

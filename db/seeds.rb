1344b8dac2519ee245746f260945d2d9923c6b83
Travis Smith <mr_travis_smith@hotmail.com>
2015-04-22 16:56:33 +0000

    update seed file and item index view with current bid

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@ Auction.create(name: "Spring Fundraiser", charity_id: 1, start: t, finish: (t + 
 
 User.create(fname: "Julian", lname: "Bishop", email: "jbishop@xtol.com")
 
-Item.create(auction_id: 1, user_id: 1, name: "piano", description: "old and out of tune", value: 2000)
+(1..10).each do |number|
+  Item.create(auction_id: 1, user_id: 1, name: "piano " + number.to_s, description: "old and out of tune", value: number * 10000)
+  Bid.create(item_id: number, user_id: 1, amount: number * 20000)
+end
+  
 
 
 

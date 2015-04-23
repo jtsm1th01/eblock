87c83ee7cb38d6de6c49afa075c9551bf8b0e5c9
Forrest Hayes <fkhayes@gmail.com>
2015-04-23 18:38:56 +0000

    Edit seed file. Minor style edits to items#index, #show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,24 @@
 Charity.create(name: "NorCal Autism Research Foundation")
 
 t = Time.now
-Auction.create(name: "Spring Fundraiser", charity_id: 1, start: t, finish: (t + 86400))
+Auction.create(      name: "Spring Fundraiser",
+               charity_id: 1,
+                    start: t,
+                   finish: (t + 86400))
 
-User.create(fname: "Julian", lname: "Bishop", email: "jbishop@xtol.com")
+User.create(fname: "Julian",
+            lname: "Bishop",
+            email: "jbishop@xtol.com")
 
 (1..10).each do |number|
-  Item.create(auction_id: 1, user_id: 1, name: "piano " + number.to_s, description: "old and out of tune", value: number * 10000)
-  Bid.create(item_id: number, user_id: 1, amount: number * 20000)
+  Item.create(auction_id: 1,
+                 user_id: 1,
+                    name: "piano " + number.to_s,
+             description: "old and out of tune",
+                   value: number * 10000)
+  Bid.create(    item_id: number,
+                 user_id: 1,
+                  amount: number * 20)
 end
   
 

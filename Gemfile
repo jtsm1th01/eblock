e13a2194b43f27e8281c4bbe673571b160d4d8fc
Dave Guck <davidguck@yahoo.com>
2015-04-28 22:45:12 +0000

    change aws gem version

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.7'
 gem "paperclip", "~>4.2"
-gem 'aws-sdk'
+gem 'aws-sdk', '< 2.0'
 
 gem 'bootstrap-sass'
 gem 'sass-rails', '~> 4.0.3'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
+  
+   has_attached_file :photo,
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 # validates :auction_id, presence: true
 # validates :user_id, presence: true
   validates :name, presence: true
@@ -11,10 +16,7 @@ class Item < ActiveRecord::Base
   has_many :bids
   accepts_nested_attributes_for :bids
   
-  has_attached_file :photo,
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+ 
 
   
   

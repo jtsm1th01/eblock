d2d981e6b019409796b0b5f0af9166fe8d5300c7
Dave Guck <davidguck@yahoo.com>
2015-04-28 21:58:56 +0000

    configuration for paperclip to use amazon s3

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.7'
 gem "paperclip", "~>4.2"
+gem 'aws-sdk'
 
 gem 'bootstrap-sass'
 gem 'sass-rails', '~> 4.0.3'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ActiveRecord::Base
-
 # validates :auction_id, presence: true
 # validates :user_id, presence: true
   validates :name, presence: true
@@ -14,7 +13,7 @@ class Item < ActiveRecord::Base
   
   has_attached_file :photo,
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png"
+                    :default_url => "/images/:style/missing.png",
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   

e4a2797b0afabe6621a026a53f4908defe74f9d8
Forrest Hayes <fkhayes@gmail.com>
2015-05-02 03:17:28 +0000

    Adjust bidding logic for items#index and #show to include starting bids and bid increments

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   
-   has_attached_file :photo,
+  has_attached_file :photo,
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
@@ -8,16 +8,19 @@ class Item < ActiveRecord::Base
   validates :user, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :value, presence: true
-  validates :value, numericality: {only_integer: true}
+  validates :value, :starting_bid, :bid_increment,
+             numericality: {only_integer: true, greater_than: 0}
 
   belongs_to :auction, inverse_of: :items
   belongs_to :user, inverse_of: :items
   has_many :bids
-  accepts_nested_attributes_for :bids
   
   def high_bid
     bids.maximum("amount")
+  end
+
+  def min_bid(going_bid)
+    bids.count == 0 ? going_bid : (going_bid + bid_increment)
   end
 
 end

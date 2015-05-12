5580a23a6f3d9d8925195ceff0ccd79f7edcf08b
Dave Guck <davidguck@yahoo.com>
2015-05-12 20:23:45 +0000

    add email confirmation for signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
   validates :fname, presence: true
   validates :lname, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

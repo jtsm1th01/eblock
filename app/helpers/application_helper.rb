fcdd181b88e69ecfd575c8dbc76dc36036c409ae
Forrest Hayes <fkhayes@gmail.com>
2015-05-27 23:42:30 +0000

    Configure app layout countdown with conditional app helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def countdown_explanation
+    if @auction_upcoming
+      'Auction Begins in:'
+    elsif @auction_in_progress
+      'Auction Ends in:'
+    elsif @auction_over
+      'Auction Has Ended'
+    end
+  end
+
+  def config_countdown
+    deadline = @auction_upcoming ? @auction.start : @auction.finish
+    deadline.to_f * 1000
+  end
 end

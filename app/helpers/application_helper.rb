7c06a7a43877539a3d83a46703fae0bcff718675
Forrest Hayes <fkhayes@gmail.com>
2015-05-28 19:16:07 +0000

    Edit countdown helpers to integrate auction status helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
   def countdown_explanation
-    if @auction_upcoming
+    if auction_upcoming?
       'Auction Begins in:'
-    elsif @auction_in_progress
+    elsif auction_in_progress?
       'Auction Ends in:'
-    elsif @auction_over
+    elsif auction_ended?
       'Auction Has Ended'
     end
   end
 
   def config_countdown
-    deadline = @auction_upcoming ? @auction.start : @auction.finish
+    deadline = auction_upcoming? ? @auction.start : @auction.finish
     deadline.to_f * 1000
   end
 end

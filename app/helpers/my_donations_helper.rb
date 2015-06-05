a8db2437a2a79cbfedd9a8001de1ed2d6d14530f
Travis Smith <mr_travis_smith@hotmail.com>
2015-06-05 03:57:10 +0000

    add result column to my_donations and assoc helpers

--- app/helpers/my_donations_helper.rb
+++ app/helpers/my_donations_helper.rb
@@ -5,5 +5,22 @@ module MyDonationsHelper
     return "Pending" if item.approved == false && item.declined == false
     return "Declined" if item.declined == true
   end
+  
+  def result(item)
+    if auction_ended?
+      return "Unsold" if item.bids.empty?
+      item.winning_bid.paid ? "Paid" : "Payment Pending"
+    end
+  end
+  
+  def buyer_email(item)
+    if item.winning_bid 
+      if item.winning_bid.paid
+        item.winning_bid.bid.user.email
+      else
+        "Will Notify"
+      end
+    end
+  end
 
 end

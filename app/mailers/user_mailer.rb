a513ba8fba906fb3a848bb48ceab3d075902051a
Travis Smith <mr_travis_smith@hotmail.com>
2015-06-10 19:20:50 +0000

    review winning_bid bidder email method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -19,8 +19,10 @@ class UserMailer < ActionMailer::Base
 
   def email_bidder_wrapup(user)
     email_setup(user)
-    @winning_bids = user.winning_bids.joins(:item).merge( Item.where(auction: @current_auction) )
-  end
+    # @winning_bids = user.winning_bids.joins(:item).merge( Item.where(auction: @current_auction) )
+    @winning_bids = user.winning_bids.includes(:item).includes(:auction).to_a
+    @winning_bids.keep_if { |wbid| wbid.item.auction == @current_auction }
+  end 
   
   def email_sponsor_wrapup
     mail(to: User.first.email, subject: 'Charity Auction Donor/Bidder List')

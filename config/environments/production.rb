3c3e835f01f9244d8db6afc304730d69c2c6ff59
Travis Smith <mr_travis_smith@hotmail.com>
2016-10-20 21:18:20 +0000

    alter s3 credentials after version rollback;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_region: ENV["AWS_S3_REGION"],
-    s3_credentials: {
-      s3_host_name: ENV["AWS_S3_HOST_NAME"],
-      bucket: ENV["AWS_S3_BUCKET"],
-      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-    }
-  } 
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+} 
 
   config.action_mailer.perform_deliveries = true
   # Don't care if the mailer can't send.
